488a50b3dfb882289f23ed8a08f14d50d178b354
Bhaskar Singh <rougesocket@gmail.com>
2025-06-22 15:01:05 +0530

    Initial Commit

---
+++ src/main/java/io/rougesocket/GoodQuotes/GoodQuotesApplication.java
@@ -0,0 +1,13 @@
+package io.rougesocket.GoodQuotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodQuotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodQuotesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/rougesocket/GoodQuotes/GoodQuotesApplicationTests.java
@@ -0,0 +1,13 @@
+package io.rougesocket.GoodQuotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodQuotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
